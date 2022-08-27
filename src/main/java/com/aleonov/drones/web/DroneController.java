7c48c49739d634fd4f3030c4be75bc3b9638fd88
Artem Leonov <arttemleonov@gmail.com>
2022-08-27 13:16:09 +0300

    added Swagger

--- src/main/java/com/aleonov/drones/web/DroneController.java
+++ src/main/java/com/aleonov/drones/web/DroneController.java
@@ -5,7 +5,10 @@ import com.aleonov.drones.dto.DroneResponseDto;
 import com.aleonov.drones.mapper.DroneResponseMapper;
 import com.aleonov.drones.service.drone.DroneFactory;
 import com.aleonov.drones.service.drone.DroneInfoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,18 +19,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping("drone")
+@Tag(name = "Drone")
 @RequiredArgsConstructor
 public class DroneController {
     private final DroneFactory droneFactory;
     private final DroneInfoService droneInfoService;
     private final DroneResponseMapper mapper;
 
+    @Operation(description = "Get all drones")
     @GetMapping
     public Mono<List<DroneResponseDto>> getAll() {
         return Mono.just(droneInfoService.getAll());
     }
 
-    @PostMapping
+    @Operation(description = "Register Drone")
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
     public DroneResponseDto register(@Valid @RequestBody DroneRegistrationDto droneRegistrationDto) {
         return mapper
                 .droneToDroneResponseDto(
@@ -35,6 +42,7 @@ public class DroneController {
                 );
     }
 
+    @Operation(description = "Get drone by ID")
     @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<ResponseEntity<DroneResponseDto>> getById(@PathVariable("id") Long id) {
         return Mono.justOrEmpty(droneInfoService.getById(id))
@@ -43,7 +51,8 @@ public class DroneController {
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
-    @GetMapping("available")
+    @Operation(description = "Get available drones for loading")
+    @GetMapping(value = "available", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<DroneResponseDto> getAvailableDrones() {
         return droneInfoService.getAvailableDronesForLoading();
     }
--- src/main/java/com/aleonov/drones/web/MedicamentController.java
+++ src/main/java/com/aleonov/drones/web/MedicamentController.java
@@ -5,26 +5,34 @@ import com.aleonov.drones.dto.MedicamentResponseDto;
 import com.aleonov.drones.mapper.MedicamentResponseMapper;
 import com.aleonov.drones.service.medicament.MedicamentFactory;
 import com.aleonov.drones.service.medicament.MedicamentInfoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("medicament")
+@RequestMapping("medication")
+@Tag(name = "Medication")
 @RequiredArgsConstructor
 public class MedicamentController {
     private final MedicamentFactory medicamentFactory;
     private final MedicamentInfoService medicamentInfoService;
     private final MedicamentResponseMapper mapper;
 
+    @Operation(description = "Get all medications")
     @GetMapping
     public List<MedicamentResponseDto> getAll() {
         return medicamentInfoService.getAll();
     }
 
-    @PostMapping
+    @Operation(description = "Create medication")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public MedicamentResponseDto create(@Valid @RequestBody MedicamentRegistrationDto medicamentRegistrationDto) {
         return mapper
                 .medicamentToMedicamentResponseDto(
